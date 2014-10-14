ddc5a69d3c9a90e07a6b1130028855ad78818a7f
Matt DeLuco <matt@deluco.net>
2014-10-13 23:16:03 -0400

    Added some documentation.

--- category.py
+++ category.py
@@ -1,14 +1,19 @@
 
 
 class Category(object):
+    """
+    Category is uesd to categorize lineitems and optionally apply a function
+    to the lineitem to perform actions such as calculating taxes or discounts.
+    """
 
     def __init__(self, name, descriptions=None, fn=lambda lineitem: 0):
         """
         Create a category to which fn() is applied for matching descriptions.
 
-        :param name:
-        :param descriptions: A list of strings to
-        :param fn: A function with parameters value and quantity
+        :param name: The category name
+        :param descriptions: A list of strings against which a lineitem
+        description is matched
+        :param fn: A function that takes a lineitem as its only argument
         :return:
         """
         self.name = name
@@ -16,6 +21,12 @@ class Category(object):
         self.fn = fn
 
     def categorize(self, lineitem):
+        """
+        Apply this category to the lineitem if its description matches.
+
+        :param lineitem:
+        """
+
         for desc in self.descriptions:
             if desc in lineitem.description:
                 lineitem.categorize(self)
@@ -23,6 +34,10 @@ class Category(object):
 
 
 class ExceptionCategory(Category):
+    """
+    ExceptionCategory will not apply fn to the lineitem if any of its
+    descriptions match the lineitem description.
+    """
 
     def __init__(self, *args, **kwargs):
         super(ExceptionCategory, self).__init__(*args, **kwargs)
--- lineitem.py
+++ lineitem.py
@@ -2,6 +2,9 @@ from decimal import Decimal
 
 
 class LineItem(object):
+    """
+    LineItem represents an item in a basket and a line on a receipt.
+    """
 
     def __init__(self, quantity, description, value):
         self.quantity = Decimal(quantity)
@@ -10,12 +13,23 @@ class LineItem(object):
         self.categories = {}
 
     def total(self):
+        """
+        Calculate the total of this line item based on quantity and applied
+        category functions.
+        :return:
+        """
         total = self.value * self.quantity
         for value in self.categories.itervalues():
             total += value
         return total
 
     def categorize(self, category):
+        """
+        Apply category to the lineitem and save the value returned from the
+        category function.
+        :param category:
+        :return:
+        """
         if category.name not in self.categories:
             self.categories[category.name] = 0
         self.categories[category.name] += category.fn(self)
