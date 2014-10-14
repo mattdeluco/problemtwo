f7158b7efee21290e18b27673f2fb9477e2551bc
Matt DeLuco <matt@deluco.net>
2014-10-13 23:18:06 -0400

    Added shell scripts to run problemtwo.py and unit tests.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python problemtwo.py input1.txt sales_tax_exceptions.txt
+echo
+python problemtwo.py input2.txt sales_tax_exceptions.txt
+echo
+python problemtwo.py input3.txt sales_tax_exceptions.txt
+echo
